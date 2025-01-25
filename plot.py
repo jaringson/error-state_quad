25af35eac42fca6236643e6a8993e0f44bf8f0c0
Jaron <jaringson@gmail.com>
2025-01-25 14:37:39 -0700

    Saving after submitting paper

--- plot.py
+++ plot.py
@@ -14,11 +14,12 @@ from tqdm import tqdm
 # mypath = []
 # onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
+plt.rcParams.update({'font.size': 20})
 fig, axs = plt.subplots(4, 1)
 # fig_x, axs_x = plt.subplots(2, 1)
 
-file_name = 'data/fpre.pickle'
-# file_name = 'data/lqr.pickle'
+# file_name = 'data/fpre.pickle'
+file_name = 'data/lqr.pickle'
 
 file = open(file_name, 'rb')
 data = pickle.load(file)
@@ -31,19 +32,22 @@ axs[0].plot(data['t'], data['vxc'], label='Commanded')
 axs[0].legend(loc='center right')
 axs[0].set_ylabel(r'${v_x}$(m)')
 axs[0].grid()
+axs[0].tick_params(axis='x', which='both', bottom=False, labelbottom=False)
 
 axs[1].plot(data['t'], data['vy'], label='Trajectory')
 axs[1].plot(data['t'], data['vyc'], label='Commanded')
-axs[1].legend(loc='center right')
+# axs[1].legend(loc='center right')
 axs[1].set_ylabel(r'${v_y}$(m)')
 axs[1].grid()
+axs[1].tick_params(axis='x', which='both', bottom=False, labelbottom=False)
 
 axs[2].plot(data['t'], data['vz'], label='Trajectory')
 axs[2].plot(data['t'], data['vzc'], label='Commanded')
-axs[2].legend(loc='center right')
+# axs[2].legend(loc='center right')
 axs[2].set_ylabel(r'${v_z}$(m)')
 axs[2].grid()
-            
+axs[2].tick_params(axis='x', which='both', bottom=False, labelbottom=False)
+           
 axs[3].plot(data['t'], data['J'])
 # axs[3].legend(loc='center right')
 axs[3].set_ylabel('J')
